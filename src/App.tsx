985722d6c2b52f3a4fd826d19ded54810845ad28
Jay Keegan <jay_keegan@icloud.com>
2023-10-09 23:34:19 +0100

    V1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useCallback } from 'react';
 import './App.css';
+import ReactAudioPlayer from 'react-audio-player';
+import moment from 'moment';
+import InfiniteScroll from 'react-infinite-scroll-component'; // Import the library
+
+type ShowType = 'gbx' | 'snt' | 'all';
 
 function App() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const showQ = urlParams.get('show') as ShowType | undefined | null || 'all';
+
+  const [data, setData] = useState<any[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const [pages, setPages] = useState<number>(1);
+  const [dateFilter, setDateFilter] = useState<string | null>(urlParams.get('date'));
+
+  const fetchData = useCallback(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/episode?limit=6&page=${page}&show=${showQ}${dateFilter ? `&date=${dateFilter}` : ''}`)
+      .then((res) => res.json())
+      .then((ndata) => {
+        const { episodes, pages } = ndata;
+        setData([...data, ...episodes]);
+        setPages(pages);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [data, page, showQ, dateFilter]);
+
+  useEffect(() => {
+    fetchData();
+  }, [page, showQ, dateFilter]);
+
+  const handleShowFilterChange = (show: ShowType) => {
+    window.location.href = `/?show=${show}${dateFilter ? `&date=${dateFilter}` : ''}`;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container' style={{ maxWidth: '85%' }}>
+      <h1 className="scottish-flag-text">GBX ARCHIVES</h1>
+
+      <div className="filter-bar">
+        <div className="filter-item">
+          <button
+            className={`filter-button ${showQ === 'all' ? 'active' : ''}`}
+            onClick={() => handleShowFilterChange('all')}
+          >
+            All Shows
+          </button>
+          <button
+            className={`filter-button ${showQ === 'gbx' ? 'active' : ''}`}
+            onClick={() => handleShowFilterChange('gbx')}
+          >
+            GBXperience
+          </button>
+          <button
+            className={`filter-button ${showQ === 'snt' ? 'active' : ''}`}
+            onClick={() => handleShowFilterChange('snt')}
+          >
+            Sunday Night Takeover
+          </button>
+        </div>
+      </div>
+      <div className="filter-bar">
+      <div className="filter-item">
+          <label>Date:</label>
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            window.location.href = `/?show=${showQ}${dateFilter ? `&date=${dateFilter}` : ''}`;
+          }}>
+            <input
+              type="date"
+              value={dateFilter || ''}
+              onChange={(e) => setDateFilter(e.target.value)}
+            />
+
+            <input type="submit" value="Submit" className='btn btn-primary' style={{ marginLeft: '10px' }} />
+            {dateFilter && <button type="button" className="btn btn-secondary" onClick={() => {window.location.href = `/?show=${showQ}`}} style={{ marginLeft: '10px' }}>Clear</button>}
+          </form>
+        </div>
+      </div>
+
+
+      <InfiniteScroll
+        dataLength={data.length}
+        next={() => {
+          setPage(page + 1);
+        }}
+        hasMore={page < pages}
+        loader={<h4>Loading...</h4>}
+      >
+        <div className={`row mt-4 ${data.length === 1 ? 'justify-content-center' : ''}`}>
+          {data.map((episode) => (
+            <div className="col-12 col-md-4 mb-4" key={episode.id}>
+              <div className="ep-card" style={{
+                backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${episode.image_url})`,
+              }}>
+                <span>{moment(episode.date).format('DD-MM-YYYY')}</span>
+                <div className="ep-card-content">
+                  <h3>{episode.title.replace('Sunday Night Takeover:', '')}</h3>
+                  <ReactAudioPlayer
+                    src={episode.mp3_url}
+                    controls
+                    controlsList="nodownload"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,30 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+// add bootstrap to head
+/**
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+ */
+const bootstrap = document.createElement('link');
+bootstrap.rel = 'stylesheet';
+bootstrap.href = 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css';
+
+const bootstrapBundle = document.createElement('script');
+bootstrapBundle.src = 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/js/bootstrap.bundle.min.js';
+
+document.head.appendChild(bootstrap);
+document.head.appendChild(bootstrapBundle);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <main>
+      <App />
+    </main>
+    <footer>
+      This website is not affiliated with Clyde 1 or GBX. All rights reserved to their respective owners. <br />
+    </footer>
   </React.StrictMode>
 );
 
